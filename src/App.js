d1843450166a7babb3fac61d85b0efba718c2dd1
asyahramcova <asyahramcova@gmail.com>
2023-01-08 14:23:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 
 import './App.css';
-import image from "./shopping.webp"
-import imageTwo from "./man.webp"
-import { GroceryList } from './GroceryList';
+import image from "./man.webp"
+import  clock from './clock.png'
+import { ToDoList } from './ToDoList';
 
 function App(){
   return (
     <div className='app'>
       <div className='container'>
-          <img src={ image } alt="shopping" width="250px"/>
+          <img src={ image } className= 'imgMan'  alt="shopping" width="250px"/>
       </div>
       <div className='container'>
-          <h1>Grocery List</h1>
+          <h1>To Do List</h1>
       </div>
-      <GroceryList/>
+      <ToDoList/>
+
       <div className='container'>
-          <img src={ imageTwo } alt="man" width="250px"/>
+          <img src={ clock } className = "imgClock" alt= "clock" width="50px"/>
       </div>
     </div>
   );
