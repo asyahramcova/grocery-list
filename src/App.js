d07c813d45db7153914883861887f238e4488769
asyahramcova <asyahramcova@gmail.com>
2023-01-08 15:13:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import { ToDoList } from './ToDoList';
 function App(){
   return (
     <div className='app'>
+
       <div className='container'>
           <img src={ image } className= 'imgMan'  alt="busyman" width="250px"/>
       </div>
+
       <div className='container'>
-          <h1>To Do List</h1>
+          <h1>To Do List</h1> 
       </div>
       <ToDoList/>
 
       <div className='container'>
           <img src={ clock } className = "imgClock" alt= "clock" width="50px"/>
       </div>
+      
     </div>
   );
 }
